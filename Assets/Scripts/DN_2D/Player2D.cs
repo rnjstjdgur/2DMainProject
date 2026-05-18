2206b8cf7f2ad3a3f7649334faf1bdfbcf999e4f
SHKwon <gkrrpdl@gmail.com>
2026-05-18 18:39:45 +0900

    [Setting] 기초 타일맵, 캐릭터 이동 구현

---
+++ Assets/Scripts/DN_2D/Player2D.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Player2D : MonoBehaviour
+{
+    [SerializeField] private float Move_Speed = 5f;
+    [SerializeField] private Vector3 Move_Direction;
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        Move_Direction = new Vector2(moveX, moveY); //.normalized
+
+        transform.Translate(Move_Direction * Move_Speed * Time.deltaTime);
+    }
+}

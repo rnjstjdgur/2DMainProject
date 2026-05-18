4f9a966acd47d18b3d3e7d2e6d5e8a040607ee49
SHKwon <gkrrpdl@gmail.com>
2026-05-18 15:31:55 +0900

    [UI] 로비 수정

--- Assets/Scripts/DN_UI/RobbyUI.cs
+++ Assets/Scripts/DN_UI/RobbyUI.cs
@@ -3,12 +3,10 @@
 public class RobbyUI : DaniTechUIBase
 {
     [SerializeField] private DaniTechUIButton Button_GameStart;
-    [SerializeField] private DaniTechUIButton Button_GameQuit;
 
     private void OnEnable()
     {
         Button_GameStart.BindOnClickButtonEvent(OnClick_GameStart);
-        Button_GameQuit.BindOnClickButtonEvent(OnClick_GameQuit);
     }
 
     public void OnClick_GameStart()

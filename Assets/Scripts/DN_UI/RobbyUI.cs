b43df1959484617ae5186c6ea0867ccc3a461baa
SHKwon <gkrrpdl@gmail.com>
2026-05-18 13:10:29 +0900

    [Setting] 로비 UI 틀 만들기

---
+++ Assets/Scripts/DN_UI/RobbyUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RobbyUI : DaniTechUIBase
+{
+    [SerializeField] private DaniTechUIButton Button_GameStart;
+    [SerializeField] private DaniTechUIButton Button_GameQuit;
+
+    private void OnEnable()
+    {
+        Button_GameStart.BindOnClickButtonEvent(OnClick_GameStart);
+        Button_GameQuit.BindOnClickButtonEvent(OnClick_GameQuit);
+    }
+
+    public void OnClick_GameStart()
+    {
+        DaniTechUIManager.Instance.CloseContentUI(DaniTechUIType.RobbyUI);
+    }
+
+    public void OnClick_GameQuit()
+    {
+        DaniTechGameManager.Inst.SaveAndEndGame();
+    }
+}

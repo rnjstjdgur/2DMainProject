36eec674cc770aff4d07180d2c70b22fd87644bf
SHKwon <gkrrpdl@gmail.com>
2026-06-05 11:43:36 +0900

    [UI] 메인UI 버튼제거

--- Assets/Scripts/DN_UI/DaniTech_MainUI.cs
+++ Assets/Scripts/DN_UI/DaniTech_MainUI.cs
@@ -3,13 +3,6 @@ using UnityEngine.UI;
 
 public class DaniTech_MainUI : DaniTechUIBase
 {
-    [Header("버튼")]
-    [SerializeField] private DaniTechUIButton Btn_MyProfile;
-    [SerializeField] private DaniTechUIButton Btn_StartBattle;
-    [SerializeField] private DaniTechUIButton Btn_MonsterSpawn;
-    [SerializeField] private DaniTechUIButton Btn_OpenInventory;
-    [SerializeField] private DaniTechUIButton Btn_OpenGameBook;
-
     [Header("시간")]
     [SerializeField] private Text Text_GameTimer;
 
@@ -17,12 +10,6 @@ public class DaniTech_MainUI : DaniTechUIBase
 
     private void OnEnable()
     {
-        Btn_MyProfile.BindOnClickButtonEvent(OnClick_OpenMyProfile);
-        Btn_StartBattle.BindOnClickButtonEvent(OnClick_StartBattle);
-        Btn_MonsterSpawn.BindOnClickButtonEvent(OnClicK_MonsterSpawn);
-        Btn_OpenInventory.BindOnClickButtonEvent(OnClick_OpenInventory);
-        Btn_OpenGameBook.BindOnClickButtonEvent(OnClick_OpenGameBook);
-
         Text_GameTimer.GetComponent<Text>();
     }
 
@@ -37,42 +24,4 @@ public class DaniTech_MainUI : DaniTechUIBase
             DaniTechUIManager.Instance.openBackContentUI(DaniTechUIType.EscUI);
         }
     }
-
-    public void OnClick_UseNormalSkill()
-    {
-        DaniTechGameObjectManager.Inst.GetLocalPlayer().UseNormalAttack();
-    }
-
-    public void OnClick_OpenInventory()
-    {
-        DaniTechUIManager.Instance.OpenInventoryPopup();
-        DaniTechGameManager.Inst.SaveData();
-    }
-    
-
-    public void OnClick_OpenGameBook()
-    {
-        DaniTechUIManager.Instance.OpenContentUI(DaniTechUIType.GameBookUI);
-    }
-
-    public void OnClick_OpenMyProfile()
-    {
-        //UIManager.Instance.OpenMyProfilePopup("character_hellena_01");
-        DaniTechUIManager.Instance.OpenInventoryPopup();
-        Debug.LogWarning("프로필 오픈");
-    }
-
-    public void OnClick_StartBattle()
-    {
-        DaniTechUIManager.Instance.OpenSimplePopup("배틀 스타트!");
-        Debug.LogWarning("배틀 스타트");
-    }
-
-    public void OnClicK_MonsterSpawn()
-    {
-        Debug.LogWarning("몬스터 스폰");
-    }
-
-
-
 }

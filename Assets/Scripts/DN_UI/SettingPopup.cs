efe41bf18fb4b94fac3d80bc42caefda8f728673
SHKwon <gkrrpdl@gmail.com>
2026-06-07 20:24:51 +0900

    [UI] 설정창 마무리

--- Assets/Scripts/DN_UI/SettingPopup.cs
+++ Assets/Scripts/DN_UI/SettingPopup.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingPopup : DaniTechUIBase
 {
@@ -20,9 +21,18 @@ public class SettingPopup : DaniTechUIBase
     [SerializeField] private DaniTechUIImageSequencer Sequencer_Fire;
     [SerializeField] private DaniTechUIImageSequencer Sequencer_Lightning;
 
+    [Header("애니메이터")]
+    [SerializeField] private Animator Animator_Tutorial;
+
     [Header("레이아웃")]
     [SerializeField] private GameObject Layout_MagicButton;
     [SerializeField] private GameObject Layout_Button;
+
+    [Header("자막")]
+    [SerializeField] private Text Text_Subtitle;
+
+    [Header("커튼용 이미지")]
+    [SerializeField] private Image Image_Curtain;
 
     private void OnEnable()
     {
@@ -34,6 +44,9 @@ public class SettingPopup : DaniTechUIBase
         Button_Fire.BindOnClickButtonEvent(OnClick_FireSequencer);
         Button_Lightning.BindOnClickButtonEvent(OnClick_LightningSequencer);
         Button_Back.BindOnClickButtonEvent(OnClick_BackToSettingButton);
+        SetLayoutActive(Layout_MagicButton, false);
+        Text_Subtitle.gameObject.SetActive(false);
+        Image_Curtain.gameObject.SetActive(true);
     }
 
     // 세팅팝업 버튼 이벤트 =========================================================
@@ -45,48 +58,71 @@ public class SettingPopup : DaniTechUIBase
 
     private void OnClick_ShowMagicButton()
     {
-        Layout_Button.SetActive(false);
-        Layout_MagicButton.SetActive(true);
+        Image_Curtain.gameObject.SetActive(true);
+        SetLayoutActive(Layout_MagicButton, true);
+        SetLayoutActive(Layout_Button, false);
     }
 
     private void OnClick_OpenTutorialSequencer()
     {
-        SetVideoNumAndStartVideo(4);
+        StopAllSequencer();
+        Animator_Tutorial.gameObject.SetActive(true);
+        Text_Subtitle.gameObject.SetActive(true);
     }
 
     // 마법 시연 영상 버튼 이벤트 =============================================
 
     private void OnClick_MagicArrowSequencer()
     {
-        SetVideoNumAndStartVideo(0);
+        StopAllSequencer();
+        Sequencer_MagicArrow.gameObject.SetActive(true);
     }
 
     private void OnClick_IceSequencer()
     {
-        SetVideoNumAndStartVideo(1);
+        StopAllSequencer();
+        Sequencer_Ice.gameObject.SetActive(true);
     }
 
     private void OnClick_FireSequencer()
     {
-        SetVideoNumAndStartVideo(2);
+        StopAllSequencer();
+        Sequencer_Fire.gameObject.SetActive(true);
     }
 
     private void OnClick_LightningSequencer()
     {
-        SetVideoNumAndStartVideo(3);
+        StopAllSequencer();
+        Sequencer_Lightning.gameObject.SetActive(true);
     }
 
     private void OnClick_BackToSettingButton()
     {
-        Layout_MagicButton.SetActive(false);
-        Layout_Button.SetActive(true);
+        SetLayoutActive(Layout_MagicButton, false);
+        SetLayoutActive(Layout_Button, true);
+        Image_Curtain.gameObject.SetActive(true);
+    }
+
+    // 레이아웃 관련 ============================================
+    private void SetLayoutActive(GameObject layout, bool isActive)
+    {
+        // 끄는 대신 크기를 0으로 줄여서 눈에 안 보이게 합니다.
+        layout.transform.localScale = isActive ? Vector3.one : Vector3.zero;
     }
 
     // 영상 재생관련 ============================================
 
-    private void SetVideoNumAndStartVideo(int videoNumber)
+    private void StopAllSequencer()
     {
-        Animator_Animation.SetInteger("videoNum", videoNumber);
+        Sequencer_MagicArrow.gameObject.SetActive(false);
+        Sequencer_Ice.gameObject.SetActive(false);
+        Sequencer_Fire.gameObject.SetActive(false);
+        Sequencer_Lightning.gameObject.SetActive(false);
+        Animator_Tutorial.gameObject.SetActive(false);
+        Image_Curtain.gameObject.SetActive(false);
+        Text_Subtitle.gameObject.SetActive(false);
     }
+
+    // 애니메이션 함수 =========================================
 
 }

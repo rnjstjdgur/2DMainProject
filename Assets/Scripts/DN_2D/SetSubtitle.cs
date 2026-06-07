c1ab31b376795d40c55666e7e91249caa0759e70
SHKwon <gkrrpdl@gmail.com>
2026-06-07 20:24:51 +0900

    [UI] 설정창 마무리

---
+++ Assets/Scripts/DN_2D/SetSubtitle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Subtitle : MonoBehaviour
+{
+    [Header("자막")]
+    [SerializeField] private Text Text_Subtitle;
+
+
+    public void SetSubtitle(string text)
+    {
+        Text_Subtitle.text = text;
+    }
+}
---
+++ Assets/Scripts/DN_2D/SubTitle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SubTitle : MonoBehaviour
+{
+    [Header("자막")]
+    [SerializeField] private Text Text_Subtitle;
+
+    public void SetSubtitle(string text)
+    {
+        Text_Subtitle.text = text;
+    }
+}
